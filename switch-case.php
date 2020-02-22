727a62a424b5766760129944fdd0e1ff7ebe4dfd
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 11:36:44 +0600

    first commit

---
+++ switch-case.php
@@ -0,0 +1,19 @@
+<?php
+$a = 22;
+
+switch ($a) {
+	case 0:
+		echo 'ZERO';
+		break;
+    case 1:
+      echo 'ONE';
+      break;
+    case 2:
+    echo 'TWO';
+       break;  
+	
+	default:
+		echo 'NOT matched';
+		break;
+}
+?>
